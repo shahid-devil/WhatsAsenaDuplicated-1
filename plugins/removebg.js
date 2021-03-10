9245d8c9ca185526866ecf1e13e255526ffcdbac
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-10 16:16:38 -0400

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -51,7 +51,7 @@ if (Config.WORKTYPE == 'private') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsAsena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Skueletor.png', mimetype: Mimetype.png});
         await load.delete();
     }));
 }
@@ -86,7 +86,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'WhatsAsena.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Skueletor.png', mimetype: Mimetype.png});
         await load.delete();
     }));
 }
