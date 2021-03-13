cd420e2b7d92d5e83f38b540881c643183357c9e
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-13 14:47:34 -0400

    Create hack.py

---
+++ plugins/hack.py
@@ -0,0 +1,72 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'ask', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage('*S*');
+    await message.sendMessage('*Se*');
+    await message.sendMessage('*Sen*');
+    await message.sendMessage('😍 *Seni* 😍');
+    
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('*Ç*');
+    await message.sendMessage('*Ço*');
+    await message.sendMessage('*Çoo*');
+    await message.sendMessage('🥰 *Çook* 🥰');
+    
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('*S*');
+    await message.sendMessage('*Se*');
+    await message.sendMessage('*Sev*');
+    await message.sendMessage('*Sevi*');
+    await message.sendMessage('*Seviy*');
+    await message.sendMessage('*Seviyo*');
+    await message.sendMessage('*Seviyor*');
+    await message.sendMessage('*Seviyoru*');
+    await message.sendMessage('💞 *Seviyorum* 💗');
+    
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('Seni ♥️ Seviyorum');
+
+}));
+
+Asena.addCommand({pattern: 'infoask', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage("*Codded by @Unique_hunter*\n*Seni seviyorum demenin güzel bir hali.*\n💻Usage: *.ask*")
+
+}));
+   
+Asena.addCommand({pattern: 'hack', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage('*Este usuario será hackeado* >:)\nPython Version: 3.6\nHacker: *Skueletor*\nWEB API: True');
+    await message.sendMessage('██╗░░██╗\n██║░░██║\n███████║\n██╔══██║\██║░░██║\n╚═╝░░╚═╝');
+    await message.sendMessage('░█████╗░\n██╔══██╗\n███████║\n██╔══██║\n██║░░██║\n╚═╝░░╚═╝');
+    await message.sendMessage('░█████╗░\n██╔══██╗\n██║░░╚═╝\n██║░░██╗\n╚█████╔╝\n░╚════╝░');
+    await message.sendMessage('██╗░░██╗\n██║░██╔╝\n█████═╝░\n██╔═██╗░\n██║░╚██╗\n╚═╝░░╚═╝');
+    await message.sendMessage('██╗\n██║\n██║\n██║\n██║\n╚═╝');
+    await message.sendMessage('███╗░░██╗\n████╗░██║\n██╔██╗██║\n██║╚████║\n██║░╚███║\n╚═╝░░╚══╝');
+    await message.sendMessage('░██████╗░\n██╔════╝░\n██║░░██╗░\n██║░░╚██╗\n╚██████╔╝\n░╚═════╝░');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('*¡El sistema de destino está siendo hackeado ahora mismo!*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 1%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 3%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 6%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 1%\nℂá𝕞𝕒𝕣𝕒: 3%');
+    await message.sendMessage('*Proceso en ejecución...*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 18%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 25%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 34%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 14%\nℂá𝕞𝕒𝕣𝕒: 16%');
+    await message.sendMessage('*Hackeando el sistema de seguridad del sistema*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 48%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 44%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 57%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 14%\nℂá𝕞𝕒𝕣𝕒: 62%');
+    await message.sendMessage('*Eliminando los bloqueos del sistema...*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 68%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 84%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 92%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 89%\nℂá𝕞𝕒𝕣𝕒: 86%');
+    await message.sendMessage('*Conectando a la cuenta de destino mediante la API WEB de WhatsApp*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 93%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 90%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 88%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 94%\nℂá𝕞𝕒𝕣𝕒: 96%');
+    await message.sendMessage('😎 *Cuenta Hackeada*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 100%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 100%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 100%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 100%\nℂá𝕞𝕒𝕣𝕒: 100%');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('*¡Información guardada en la base de datos de Skueletor!*');
+
+}));
+
+Asena.addCommand({pattern: 'infohack', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage("*Configurado por Skueletor*\n*Comando para hackear a alguien*\n💻Uso: */hack*")
+
+}));
