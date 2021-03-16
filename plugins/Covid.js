825cab36583065e6034d9b97cad358ce3d691d00
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-16 17:03:46 -0400

    Update Covid.js

--- plugins/Covid.js
+++ plugins/Covid.js
@@ -6,7 +6,7 @@ const cn = require('../config');
 if (cn.WORKTYPE == 'public') {
     
     Asena.addCommand({pattern: "covid ?(.*)", fromMe: true}, (async (message, match) => {
-    else if (match[1] === "") {
+    if (match[1] === "") {
         try{
             //const resp = await fetch("https://coronavirus-19-api.herokuapp.com/all").then(r => r.json());
             const respo = await got("https://coronavirus-19-api.herokuapp.com/all").then(async ok => {
