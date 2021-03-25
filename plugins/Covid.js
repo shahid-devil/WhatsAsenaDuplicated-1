4b0a16dd709a4764621ca0a4b2dd75cf86683d1c
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-24 21:12:39 -0400

    Update Covid.js

--- plugins/Covid.js
+++ plugins/Covid.js
@@ -35,7 +35,7 @@ if (con.WORKTYPE == 'public') {
         try{
             const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Spain").then(async ok  => {
                 resp = JSON.parse(ok.body);
-                await message.reply(`🇧🇴 *Datos de Bolivia:*\n😷 *Casos en Total:* ${resp.cases}\n🏥 *Casos diarios:* ${resp.todayCases}\n⚰️ *Muertes en total:* ${resp.deaths}\n☠️ *Muertes diarias:* ${resp.todayDeaths}\n💊 *Recuperados en total:* ${resp.recovered}\n😷 *Casos activos:* ${resp.active}\n🆘 *Casos críticos:* ${resp.critical}\n🧪 *Pruebas totales:* ${resp.totalTests}`);
+                await message.reply(`🇪🇸 *Datos de España:*\n😷 *Casos en Total:* ${resp.cases}\n🏥 *Casos diarios:* ${resp.todayCases}\n⚰️ *Muertes en total:* ${resp.deaths}\n☠️ *Muertes diarias:* ${resp.todayDeaths}\n💊 *Recuperados en total:* ${resp.recovered}\n😷 *Casos activos:* ${resp.active}\n🆘 *Casos críticos:* ${resp.critical}\n🧪 *Pruebas totales:* ${resp.totalTests}`);
             });
         } catch (err) {
             await message.reply(`Bir Hata Oluştu, İşte Hata : \n${err.message}`, MessageType.text)
