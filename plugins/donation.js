d817f720c4428917b30ef6a3a3fc41bc5b40d4ad
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-24 20:36:07 -0400

    Update donation.js

--- plugins/donation.js
+++ plugins/donation.js
@@ -8,7 +8,7 @@ Asena.addCommand({pattern: 'donate', fromMe: false}, (async (message, match) => 
 
     await message.sendMessage('Hola, soy *Skueletor*.');
     await new Promise(r => setTimeout(r, 800));
-    await message.sendMessage('¡Hola, me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
+    await message.sendMessage('¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
     await new Promise(r => setTimeout(r, 1650));
     await message.sendMessage('Por cierto, muy pronto los donadores tendrán beneficios dentro del bot...\nAsí que si decides apoyar este proyecto, cuando hagas tu donación envíame una captura de pantalla por Telegram, t.me/DKzippO\nCuando me escribas, dime cuál es tu número de teléfono con el que utilizas el bot y te tomaré en cuenta.');
     }));
@@ -21,7 +21,7 @@ Asena.addCommand({pattern: 'donate', fromMe: true}, (async (message, match) => {
 
     await message.sendMessage('Hola, soy *Skueletor*.');
     await new Promise(r => setTimeout(r, 800));
-    await message.sendMessage('¡Hola, me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
+    await message.sendMessage('¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
     await new Promise(r => setTimeout(r, 1650));
     await message.sendMessage('Por cierto, muy pronto los donadores tendrán beneficios dentro del bot...\nAsí que si decides apoyar este proyecto, cuando hagas tu donación envíame una captura de pantalla por Telegram, t.me/DKzippO\nCuando me escribas, dime cuál es tu número de teléfono con el que utilizas el bot y te tomaré en cuenta.');
     }));
