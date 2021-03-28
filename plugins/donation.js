dab373766b8a1b2aad3022d32b272c1fad061073
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-28 00:23:04 -0400

    Update donation.js

--- plugins/donation.js
+++ plugins/donation.js
@@ -18,7 +18,7 @@ const con = require('../config');
         await message.client.sendMessage(
             message.jid, 
             fs.readFileSync("https://telegra.ph/file/470496095696fab0eb394.jpg"),
-            MessageType.video, 
+            MessageType.photo, 
             { mimetype: Mimetype.photo, caption: `¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.` }
           )
   
