8a090fbe3a3c5aab88c4ea519dbe3c00e3d9761b
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-28 00:22:37 -0400

    Update donation.js

--- plugins/donation.js
+++ plugins/donation.js
@@ -7,11 +7,20 @@ const con = require('../config');
   
     Asena.addCommand({pattern: 'donation', fromMe: true}, (async (message, match) => {
 
-        await message.sendMessage('Hola, soy *Skueletor*.');
+      
+       await message.client.sendMessage(message.jid, '¡Hola' + '@' + message.reply_message.jid.split('@')[0] + ', soy *Skueletor*!', MessageType.text, {
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+
+            });
   
         await new Promise(r => setTimeout(r, 800));
-  
-        await message.sendMessage('¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
+      
+        await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("https://telegra.ph/file/470496095696fab0eb394.jpg"),
+            MessageType.video, 
+            { mimetype: Mimetype.photo, caption: `¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.` }
+          )
   
         await new Promise(r => setTimeout(r, 1650));
   
@@ -23,7 +32,7 @@ const con = require('../config');
   
     Asena.addCommand({pattern: 'donation', fromMe: false}, (async (message, match) => {
 
-        await message.sendMessage('Hola, soy *Skueletor*.');
+       await message.sendMessage('Hola, soy *Skueletor*.');
   
        await new Promise(r => setTimeout(r, 800));
   
