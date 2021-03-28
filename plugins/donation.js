4b207f3c250117c6a88d67a0582a7f165411ccd4
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-27 23:37:51 -0400

    Update donation.js

--- plugins/donation.js
+++ plugins/donation.js
@@ -9,9 +9,13 @@ if (con.LANG === 'ES') {
 Asena.addCommand({pattern: 'donation', fromMe: true}, (async (message, match) => {
 
     await message.sendMessage('Hola, soy *Skueletor*.');
+  
     await new Promise(r => setTimeout(r, 800));
+  
     await message.sendMessage('¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
+  
     await new Promise(r => setTimeout(r, 1650));
+  
     await message.sendMessage('Por cierto, muy pronto los donadores tendrán beneficios dentro del bot...\nAsí que si decides *apoyar* este proyecto, cuando hagas tu donación envíame una captura de pantalla por Telegram, t.me/DKzippO\nCuando me escribas, dime cuál es tu número de teléfono con el que utilizas el bot y te tomaré en cuenta.');
   
     }));
@@ -21,9 +25,13 @@ else if (con.WORKTYPE == 'public') {
 Asena.addCommand({pattern: 'donation', fromMe: false}, (async (message, match) => {
 
     await message.sendMessage('Hola, soy *Skueletor*.');
+  
     await new Promise(r => setTimeout(r, 800));
+  
     await message.sendMessage('¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.');
+  
     await new Promise(r => setTimeout(r, 1650));
+  
     await message.sendMessage('Por cierto, muy pronto los donadores tendrán beneficios dentro del bot...\nAsí que si decides *apoyar* este proyecto, cuando hagas tu donación envíame una captura de pantalla por Telegram, t.me/DKzippO\nCuando me escribas, dime cuál es tu número de teléfono con el que utilizas el bot y te tomaré en cuenta.');
 
     }));
