db9541233bd7c6df98f5303f7971a807b504d467
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-28 01:09:28 -0400

    Update donation.js

--- plugins/donation.js
+++ plugins/donation.js
@@ -1,6 +1,8 @@
 const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const axios = require('axios')
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const con = require('../config');
+const fs = require('fs');
 
   
   if (con.WORKTYPE == 'private') {
@@ -15,12 +17,9 @@ const con = require('../config');
   
         await new Promise(r => setTimeout(r, 800));
       
-        await message.client.sendMessage(
-            message.jid, 
-            url: ("https://telegra.ph/file/470496095696fab0eb394.jpg"),
-            MessageType.photo, 
-            { mimetype: Mimetype.photo, caption: `¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.` }
-          )
+          var image = await axios.get ('https://telegra.ph/file/470496095696fab0eb394.jpg', {responseType: 'arraybuffer'})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg}, caption: `¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.` )
+
   
         await new Promise(r => setTimeout(r, 1650));
   
@@ -40,12 +39,9 @@ const con = require('../config');
   
         await new Promise(r => setTimeout(r, 800));
       
-        await message.client.sendMessage(
-            message.jid, 
-            url: ("https://telegra.ph/file/470496095696fab0eb394.jpg"),
-            MessageType.photo, 
-            { mimetype: Mimetype.photo, caption: `¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.` }
-          )
+          var image = await axios.get ('https://telegra.ph/file/470496095696fab0eb394.jpg', {responseType: 'arraybuffer'})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg}, caption: `¡Me alegra saber que quieres donar!\nSkueletor es un proyecto nuevo que necesita apoyo, te dejo el enlace de PayPal: paypal.me/proyectomenacho por si quieres ayudar con este nuevo proyecto, de antemano, *muchas gracias* ❤️.` )
+
   
         await new Promise(r => setTimeout(r, 1650));
   
