bff6aff4b059985e4c18077cf8ea3a80e5ebf9e0
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-03-30 14:47:45 -0400

    Update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -1,8 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 Thanks to github/@justinthoms for base and helps.
 */
@@ -17,7 +15,8 @@ const Lang = Language.getString('ocr');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC}, (async (message, match) => { 
+
         if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
 	var info = await message.reply(Lang.DOWNLOADING);
         var location = await message.client.downloadAndSaveMediaMessage({
@@ -45,7 +44,7 @@ if (Config.WORKTYPE == 'private') {
 
         await info.delete();
         if ( result === ' ' || result.length == 1 ) {
-            return await message.reply(Lang.ERROR.format(' No se encontró texto...'));
+            return await message.reply(Lang.ERROR.format(' Empty text'));
         }
 
         return await message.reply(Lang.RESULT.format(dil[2], result));
@@ -53,7 +52,8 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: false, desc: Lang.OCR_DESC}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: false, desc: Lang.OCR_DESC}, (async (message, match) => { 
+
         if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
 	var info = await message.reply(Lang.DOWNLOADING);
         var location = await message.client.downloadAndSaveMediaMessage({
@@ -81,7 +81,7 @@ else if (Config.WORKTYPE == 'public') {
 
         await info.delete();
         if ( result === ' ' || result.length == 1 ) {
-            return await message.reply(Lang.ERROR.format(' No se encontró texto...'));
+            return await message.reply(Lang.ERROR.format(' Empty text'));
         }
 
         return await message.reply(Lang.RESULT.format(dil[2], result));
