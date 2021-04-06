870c26c218a5db41ac9a36d2e03761af6b5dbbae
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-04-06 00:43:02 -0400

    Create emojis1.js

---
+++ plugins/emojis1.js
@@ -0,0 +1,59 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+// Languages
+const END = "Ese es un complemento con herramientas agradables juntas en su interior. (Paquete de emojis V1)"
+
+        Asena.addCommand({pattern: 'emojis1', fromMe: true, desc: END}, (async (message, match) => {
+
+            await message.sendMessage("_Ese es un complemento con herramientas agradables juntas en su interior._\n💻Uso: */cumple*\nℹ️Descripción: *Celebra un cumpleaños.*\n\n💻Uso: */guerra*\nℹ️Descripción: *¿Qué tal un pequeño trago, amigo?*\n\n💻Uso: */amor*\nℹ️Descripción: *Una forma diferente de decir te amo*\n\n💻Uso: */helicoptero*\nℹ️Descripción: *¿Qué tal aterrizar en un helicóptero y saludar?*\n\n💻Uso: */corazon*\nℹ️Descripción: *Envíale un corazón a alguien.*\n\n💻Uso: */tanque*\nℹ️Descripción: *¿Qué tal enviar un tanque?*\n\n💻Uso: */luna*\nℹ️Descripción: *Para aquellos que arden con el amor de la luna*\n\n💻Uso: */gato*\nℹ️Descripción: *¿Qué tal enviar un gato para animar la charla?*\n\nCodded by *@DKzippO*\nVersión: *1.0*");
+
+        }));
+
+        Asena.addCommand({pattern: 'cumple', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("\n♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪\n░F░E░L░I░Z░♪░C░U░M░P░L░E░A░Ñ░O░S░\n♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪");
+
+        }));
+
+        Asena.addCommand({pattern: 'guerra', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("\n_/﹋\_\n(҂`_´)\n<,︻╦╤─ ҉ - -\n_/﹋\_");
+
+        }));
+
+        Asena.addCommand({pattern: 'amor', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("╔══╗╔╗ ♡ ♡ ♡\n╚╗╔╝║║╔═╦╦╦╔╗\n╔╝╚╗║╚╣║║║║╔╣\n╚══╝╚═╩═╩═╩═╝\nஜ۞ஜ YOU ஜ۞ஜ");
+
+        }));
+
+        Asena.addCommand({pattern: 'helicoptero', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage(" ▬▬▬.◙.▬▬▬\n ═▂▄▄▓▄▄▂\n◢◤ █▀▀████▄▄▄▄◢◤\n█▄▂█ █▄███▀▀▀▀▀▀▀╬\n◥█████◤\n══╩══╩═\n ╬═╬\n ╬═╬\n ╬═╬\n ╬═╬\n ╬═╬\n ╬═╬ ☻/\n ╬═╬/▌\n ╬═╬/");
+
+        }));
+
+        Asena.addCommand({pattern: 'corazon', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("(っ◔◡◔っ) ❤");
+
+        }));
+
+        Asena.addCommand({pattern: 'tanque', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("  ░░░░░░███████ ]▄▄▄▄▄▄▄▄          \n▂▄▅█████████▅▄▃▂   \nIl███████████████████].   ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤..\n");
+  
+        }));
+
+        Asena.addCommand({pattern: 'luna', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑\n🌑🌑🌒🌘🌑🌑🌑🌒🌘🌑🌑\n🌑🌔🌕🌕🌕🌑🌕🌕🌕🌖🌑\n🌑🌕🌕🌕🌕🌕🌕🌕🌕🌕🌑\n🌑🌔🌕🌕🌕🌕🌕🌕🌕🌖🌑\n🌑🌒🌕🌕🌕🌕🌕🌕🌕🌘🌑\n🌑🌑🌒🌕🌕🌕🌕🌕🌘🌑🌑\n🌑🌑🌒🌕🌕🌕🌕🌕🌘🌑🌑\n🌑🌑🌑🌒🌕🌕🌕🌘🌑🌑🌑\n🌑🌑🌑🌑🌒🌕🌘🌑🌑🌑🌑\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑\n🌑🌑🌒🌘🌑🌑🌑🌒🌘🌑🌑\n🌑🌒❤️❤️🌘🌑🌒❤️❤️🌘🌑\n🌑🌔❤️❤️🌕🌑❤️❤️❤️🌖🌑\n🌑❤️❤️❤️❤️❤️❤️❤️❤️❤️🌑\n🌑🌔❤️❤️❤️❤️❤️❤️❤️🌖🌑\n🌑🌒❤️❤️❤️❤️❤️❤️❤️🌘🌑\n🌑🌑🌒❤️❤️❤️❤️❤️🌘🌑🌑\n🌑🌑🌑🌒❤️❤️❤️🌘🌑🌑🌑\n🌑🌑🌑🌑🌒🌕🌘🌑🌑🌑🌑\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑");
+
+        }));
+
+        Asena.addCommand({pattern: 'gato', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+            await message.sendMessage("──────▄▀▄─────▄▀▄\n─────▄█░░▀▀▀▀▀░░█▄\n─▄▄──█░░░░░░░░░░░█──▄▄\n█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█");
+
+        }));
