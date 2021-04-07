c03de5e57a5deeb61566a593deb10954caf21927
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-04-06 23:22:13 -0400

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -20,11 +20,30 @@ if (Config.WORKTYPE == 'private') {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
-        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true&delay=400`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Hecho por *Skueletor*'})
 
     }));
+    
+    const sh = "Genera rostros humanos que antes no existían con inteligencia artificial."
+Asena.addCommand({pattern: 'faceai', fromMe: true, desc: sh}, (async (message, match) => {
+
+    var webimage = await axios.get('https://screenshotapi.net/api/v1/screenshot?url=https://thispersondoesnotexist.com/&output=image&width=1000&height=1000', { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Hecho por *Skueletor*'})
+
+}));
+
+const dh = "Genera caras de anime que no existían antes con inteligencia artificial."
+Asena.addCommand({pattern: 'animai', fromMe: true, desc: dh}, (async (message, match) => {
+
+    var webimage = await axios.get('https://screenshotapi.net/api/v1/screenshot?url=https://www.thiswaifudoesnotexist.net/&output=image&width=1000&height=1000&retina=true&full_page=false', { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Hecho por *Skueletor*'})
+
+}));
+
 }
 else if (Config.WORKTYPE == 'public') {
 
@@ -32,9 +51,27 @@ else if (Config.WORKTYPE == 'public') {
 
         if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
-        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true&delay=400`, { responseType: 'arraybuffer' })
+        var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Hecho por *Skueletor*'})
 
     }));
+    
+    const sh = "Genera rostros humanos que antes no existían con inteligencia artificial."
+Asena.addCommand({pattern: 'faceai', fromMe: false, desc: sh}, (async (message, match) => {
+
+    var webimage = await axios.get('https://screenshotapi.net/api/v1/screenshot?url=https://thispersondoesnotexist.com/&output=image&width=1000&height=1000', { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Hecho por *Skueletor*'})
+
+}));
+
+const dh = "Genera caras de anime que no existían antes con inteligencia artificial."
+Asena.addCommand({pattern: 'animai', fromMe: false, desc: dh}, (async (message, match) => {
+
+    var webimage = await axios.get('https://screenshotapi.net/api/v1/screenshot?url=https://www.thiswaifudoesnotexist.net/&output=image&width=1000&height=1000&retina=true&full_page=false', { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Hecho por *Skueletor*'})
+
+}));
 }
