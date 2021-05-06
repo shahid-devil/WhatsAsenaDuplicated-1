a63a9b2bdd620bf770afd4405e4ab72ea90b3629
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-05-06 16:30:53 -0400

    Update web.js

--- plugins/web.js
+++ plugins/web.js
@@ -44,10 +44,9 @@ Asena.addCommand({pattern: 'speedtest', fromMe: true, desc: Lang.SPEEDTEST_DESC}
 
 Asena.addCommand({pattern: 'ping', fromMe: true, deleteCommand: false, desc: Lang.PING_DESC}, (async (message, match) => {
   var start = new Date().getTime();
-  var msg = await message.reply('```¡Ping!```');
+  await message.sendMessage('```¡Ping!```');
   var end = new Date().getTime();
 
-  await msg.delete();
   await message.client.sendMessage(
     message.jid,'*¡Pong!*\n```' + (end - start) + 'ms```', MessageType.text);
 }));
@@ -60,10 +59,37 @@ if (Config.WORKTYPE == 'private') {
 
         TinyURL.shorten(`${match[1]}`, async(res, err) => {
           if (err)
-            await message.client.sendMessage(message.jid, '*#### Error! ####*\n\n' + '```' + err + '```', MessageType.text);
+            await message.client.sendMessage(message.jid, '*#### ¡Error! ####*\n\n' + '```' + err + '```', MessageType.text);
 
             await message.client.sendMessage(message.jid,`*Enlace Original:* ${match[1]}\n*Enlace Corto:* ` + res, MessageType.text)
         });
+    }));
+    Asena.addCommand({pattern: 'calc ?(.*)', fromMe: true, desc: Lang.CALC }, (async (message, match) => {
+        if (match[1].length < 4) { return await message.client.sendMessage(message.jid,Lang.VALİD, MessageType.text) }
+        if (match[1].includes('+')) { var split = match[1].split('+'), sonsayi = split[1], ilksayi = split[0]
+            var result = -(-ilksayi - sonsayi)
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('-')) { var split = match[1].split('-'), sonsayicik = split[1], ilksayicik = split[0] 
+            var result = ilksayicik - sonsayicik
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('x')) { var split = match[1].split('x'), sonsayicarp = split[1], ilksayicarp = split[0] 
+            var result = ilksayicarp * sonsayicarp
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('/')) { var split = match[1].split('/'), sonsayibol = split[1], ilksayibol = split[0] 
+            var result = ilksayibol / sonsayibol
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text)
+            }
+        }
     }));
 }
 else if (Config.WORKTYPE == 'public') {
@@ -74,9 +100,74 @@ else if (Config.WORKTYPE == 'public') {
 
         TinyURL.shorten(`${match[1]}`, async(res, err) => {
           if (err)
-            await message.client.sendMessage(message.jid, '*#### Error! ####*\n\n' + '```' + err + '```', MessageType.text);
+            await message.client.sendMessage(message.jid, '*#### ¡Error! ####*\n\n' + '```' + err + '```', MessageType.text);
 
             await message.client.sendMessage(message.jid,`*Enlace Original:* ${match[1]}\n*Enlace Corto:* ` + res, MessageType.text)
         });
+    }));
+    Asena.addCommand({pattern: 'short ?(.*)', fromMe: true, desc: Lang.URL, dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid, SLang.LİNK, MessageType.text);
+
+        TinyURL.shorten(`${match[1]}`, async(res, err) => {
+          if (err)
+            await message.client.sendMessage(message.jid, '*#### ¡Error! ####*\n\n' + '```' + err + '```', MessageType.text);
+
+            await message.client.sendMessage(message.jid,`*Enlace Original:* ${match[1]}\n*Enlace Corto:* ` + res, MessageType.text)
+        });
+    }));
+    Asena.addCommand({pattern: 'calc ?(.*)', fromMe: false, desc: Lang.CALC }, (async (message, match) => {
+        if (match[1].length < 4) { return await message.client.sendMessage(message.jid,Lang.VALİD, MessageType.text) }
+        if (match[1].includes('+')) { var split = match[1].split('+'), sonsayi = split[1], ilksayi = split[0]
+            var result = -(-ilksayi - sonsayi)
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('-')) { var split = match[1].split('-'), sonsayicik = split[1], ilksayicik = split[0] 
+            var result = ilksayicik - sonsayicik
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('x')) { var split = match[1].split('x'), sonsayicarp = split[1], ilksayicarp = split[0] 
+            var result = ilksayicarp * sonsayicarp
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('/')) { var split = match[1].split('/'), sonsayibol = split[1], ilksayibol = split[0] 
+            var result = ilksayibol / sonsayibol
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text)
+            }
+        }
+    }));
+    Asena.addCommand({pattern: 'calc ?(.*)', fromMe: true, desc: Lang.CALC, dontAddCommandList: true }, (async (message, match) => {
+        if (match[1].length < 4) { return await message.client.sendMessage(message.jid,Lang.VALİD, MessageType.text) }
+        if (match[1].includes('+')) { var split = match[1].split('+'), sonsayi = split[1], ilksayi = split[0]
+            var result = -(-ilksayi - sonsayi)
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('-')) { var split = match[1].split('-'), sonsayicik = split[1], ilksayicik = split[0] 
+            var result = ilksayicik - sonsayicik
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('x')) { var split = match[1].split('x'), sonsayicarp = split[1], ilksayicarp = split[0] 
+            var result = ilksayicarp * sonsayicarp
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
+            }
+        }
+        else if (match[1].includes('/')) { var split = match[1].split('/'), sonsayibol = split[1], ilksayibol = split[0] 
+            var result = ilksayibol / sonsayibol
+            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
+            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text)
+            }
+        }
     }));
 }
