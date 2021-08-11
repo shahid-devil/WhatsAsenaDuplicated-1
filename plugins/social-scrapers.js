9b41c29e0a8fc3e940dd088d28827c3ce87cdd5c
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-08-10 23:35:58 -0400

    Update social-scrapers.js

--- plugins/social-scrapers.js
+++ plugins/social-scrapers.js
@@ -7,6 +7,8 @@ const Asena = require('../events')
 const { MessageType } = require('@adiwajshing/baileys')
 const axios = require('axios')
 const Config = require('../config');
+const got = require('got');
+const fs = require('fs');
 
 const Language = require('../language')
 const { errorMessage, infoMessage } = require('../helpers')
@@ -14,33 +16,64 @@ const Lang = Language.getString('instagram')
 const Tlang = Language.getString('tiktok')
 const Ierr = "¡No encontré ningún Tiktok! :("
 const Wath = "¡Necesito que ingreses un enlace!"
-const Ivd = "¡Descargando y enviando el video!"
+const NEED_WORD = "Por favor, ingresa el enlace."
+const FBDESC = "Descarga videos desde Facebook"
+const LOADING = "Descargando el video..."
+const NOT_FOUNDFB = "Video no encontrado."
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: true}, async (message, match) => {
+    const userName = match[1]
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+    await message.sendMessage(infoMessage("Descargando el post..."))
+    await axios
+      .get(`https://api-anoncybfakeplayer.herokuapp.com/igdown?url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+        const msg = `${type}`
+	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: "Hecho por *Skueletor*"
+        })}
+		 	 
+	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: "Hecho por *Skueletor*"
+        })}
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=e67bd1bafe81b611`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
-
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
-
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
-
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
-    }));
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("Enlace no válido, ingrese un enlace de Instagram válido")),
+      )
+  },
+)
+	
+	Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: true, desc: FBDESC }, async (message, match) => {
+    const userName = match[1]
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+    await message.sendMessage(infoMessage(LOADING))
+    await axios
+      .get(`https://videfikri.com/api/fbdl/?urlfb=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          judul,
+        } = response.data.result
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+        const msg = `*${CAPTION}*: ${judul}`
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: "Hecho por *Skueletor*"
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(NOT_FOUNDFB )),
+      )
+  },
+)
 
 	Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true}, async (message, match) => {
 
@@ -66,29 +99,57 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, (async (message, match) => {
-        if (match[0].includes('install')) return;
-        if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
-        if (!match[1].includes('www.instagram.com')) return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text, { quoted: message.data });
+Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false}, async (message, match) => {
+    const userName = match[1]
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+    await message.sendMessage(infoMessage("Descargando el post..."))
+    await axios
+      .get(`https://api-anoncybfakeplayer.herokuapp.com/igdown?url=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          type,
+        } = response.data.result[0]
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+        const msg = `${type}`
+	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: "Hecho por *Skueletor*"
+        })}
+		 	 
+	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: "Hecho por *Skueletor*"
+        })}
 	
-        let urls = `https://api.xteam.xyz/dl/ig?url=${match[1]}&APIKEY=e67bd1bafe81b611`
-        let response
-        try { response = await got(urls) } catch { return await message.client.sendMessage(message.jid, Lang.FİX, MessageType.text, { quoted: message.data });
-        }
-        const json = JSON.parse(response.body);
-
-        if (json.status === false) return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text, { quoted: message.data });
-        if (json.code === 403) return await message.client.sendMessage(message.jid, '```API Error!```', MessageType.text, { quoted: message.data });
-
-        await message.client.sendMessage(message.jid, Tlang.DOWN, MessageType.text, { quoted: message.data });
-
-        let url = json.result.data[0].data;
-        let name = json.result.data[0].type;
-        await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
-            }
-        });
-    }));
+        
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("Enlace no válido, ingrese un enlace de Instagram válido")),
+      )
+  },
+)
+	
+	Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (message, match) => {
+    const userName = match[1]
+    if (!userName) return await message.sendMessage(errorMessage(NEED_WORD))
+    await message.sendMessage(infoMessage(LOADING))
+    await axios
+      .get(`https://videfikri.com/api/fbdl/?urlfb=${userName}`)
+      .then(async (response) => {
+        const {
+          url,
+          judul,
+        } = response.data.result
+        const profileBuffer = await axios.get(url, {responseType: 'arraybuffer'})
+        const msg = `*${CAPTION}*: ${judul}`
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
+          caption: "Hecho por *Skueletor*"
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage(NOT_FOUNDFB )),
+      )
+  },
+)
 		
 	Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: false}, async (message, match) => {
 
