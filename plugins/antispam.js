07dfa47eafebf29307afcff928d0158cf30d1bf5
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-08-10 22:40:59 -0400

    Update antispam.js

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -5,10 +5,10 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Ln = ('Este comando para cualquier situación de emergencia sobre cualquier tipo de SPAM de WhatsApp en Grupo');
 
 const code = ('A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n *🎭 Skueletor*')
-const FINAL = "```⚠️Este es un antispam (anti-lag) Este código evitará que su teléfono se atasque debido a mensajes maliciosos.⚠️```"
+const FINAL = "```⚠️Este es un antispam (anti-lag).\n Este código evitará que su teléfono se bloquee debido a mensajes maliciosos.⚠️```"
 const TMUTE = "Intentando cerrar el grupo temporalmente..."
 const MUT = "/mute 1d"
-const SCXR = "```♻️ Comando de inicio... ♻️```"
+const SCXR = "```Comando iniciado... ♻️```"
 Asena.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
 
   var msg = await message.reply('```☢ Por orden de Skueletor, el grupo se libra de todo peligro... ☢```');
