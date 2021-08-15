15d926c4c3dfa61e71e4d5778d31c3a92779493d
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2021-08-15 00:13:24 -0400

    Create id.js

---
+++ plugins/id.js
@@ -0,0 +1,46 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const fs = require('fs');
+
+const WAME_DESC = "Get a link to the user chat."
+const WAME = "```Enlace de chat de```@{}: https://wa.me/{}"
+const NEED_UWONG = "*¡Menciona a un usuario!*"
+
+if (config.WORKTYPE === 'private') {
+
+      Asena.addCommand({pattern: 'wame ?(.*)', fromMe: true, onlyGroup: true, desc: WAME_DESC}, (async (message, match) => {    
+        if (message.reply_message !== false) {
+            await message.client.sendMessage(message.jid, WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid.replace('@s.whatsapp.net', ' ')), MessageType.text, {
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+            });
+        } else if (message.mention !== false) {
+            message.mention.map(async user => {
+                await message.client.sendMessage(message.jid, WAME.format(user.split('@')[0], user.replace('@s.whatsapp.net', ' ')), MessageType.text, {
+                    contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
+                }); 
+            });
+        } else {
+            await message.client.sendMessage(message.jid, NEED_UWONG, MessageType.text);
+        }
+    }));
+}
+
+else if (con.WORKTYPE === 'public') {
+
+      Asena.addCommand({pattern: 'wame ?(.*)', fromMe: false, onlyGroup: true, desc: WAME_DESC}, (async (message, match) => {    
+        if (message.reply_message !== false) {
+            await message.client.sendMessage(message.jid, WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid.replace('@s.whatsapp.net', ' ')), MessageType.text, {
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+            });
+        } else if (message.mention !== false) {
+            message.mention.map(async user => {
+                await message.client.sendMessage(message.jid, WAME.format(user.split('@')[0], user.replace('@s.whatsapp.net', ' ')), MessageType.text, {
+                    contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
+                }); 
+            });
+        } else {
+            await message.client.sendMessage(message.jid, NEED_UWONG, MessageType.text);
+        }
+    }));
+}
