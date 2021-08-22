051d76bab3310c001c46d19f3f61b5d79a1ba8d0
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2021-08-22 19:28:55 -0400

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,23 +1,22 @@
-const Skueletor = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require("@adiwajshing/baileys");
 const id = '51912545279@s.whatsapp.net' // the WhatsApp ID 
 
-const buttons = [
-  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
-  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1}
+const rows = [
+ {title: 'Test 1', description: "Hola esta es la prueba de descripción 1", rowId:"rowid1"},
+ {title: 'Test 2', description: "Hola esta es la prueba de descripción 2", rowId:"rowid2"}
 ]
 
-const buttonMessage = {
-    contentText: "Hola, es el mensaje con botones",
-    footerText: 'Hello World',
-    buttons: buttons,
-    headerType: 1
-}
+const sections = [{title: "Skueletor", rows: rows}]
 
-await conn.relayWAMessage(WAMessageProto.ButtonsMessage.fromObject(buttonMessage), {})
+const button = {
+ buttonText: '¡Toca aquí!',
+ description: "Hola, esta es la lista de mensajes.",
+ sections: sections,
+ listType: 1
+}
 
   Skueletor.addCommand({pattern: 'test', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 
-    await conn.sendMessage(id, buttonMessage, MessageType.buttonsMessage)
+      await conn.sendMessage(id, buttonMessage, MessageType.listMessage)
 
         }));
