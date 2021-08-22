7f1c4582196bec26e770b31af21fec7d78acb864
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2021-08-22 19:34:27 -0400

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,4 +1,6 @@
 const {MessageType, MessageOptions, Mimetype} = require("@adiwajshing/baileys");
+const Asena = require('../events');
+const Config = require('../config');
 const id = '51912545279@s.whatsapp.net' // the WhatsApp ID 
 
 const rows = [
