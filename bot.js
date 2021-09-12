27427a01c67192cddbe7a3897ff56726e6216d89
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2021-09-12 06:07:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ async function whatsAsena () {
                 var ov_time = new Date().toLocaleString('ES', { timeZone: 'Europe/Madrid' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\nHora de España 🇪🇸\n\nby Skueletor 🐺\n\nPor favor, ten en cuenta que es un proyecto que aún está en desarrollo, lo que significa que puede contener fallas 🚧'
+                const biography = 'Por favor, ten en cuenta que es un proyecto que aún está en desarrollo, lo que significa que puede contener fallas 🚧            Hora de España 🇪🇸            📅 ' + utch + '\n⌚ ' + ov_time + 'by Skueletor 🐺'
                 await conn.setStatus(biography)
             }
     }, 7890);
