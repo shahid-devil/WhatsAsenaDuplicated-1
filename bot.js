7a252e2be7553eea95feedb7e7ff3546ec1526fd
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2021-09-11 23:50:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2021
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-DrkBot - Ian VanH
+Skueletor
 */
 
 const fs = require("fs");
