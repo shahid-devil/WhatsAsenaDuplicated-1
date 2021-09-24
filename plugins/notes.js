bcdd409db0d8912592fe48f7644afa5dd1bae097
Skueletor <73276191+SkueletorTlg@users.noreply.github.com>
2021-09-23 20:17:50 -0400

    Update notes.js

--- plugins/notes.js
+++ plugins/notes.js
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-*/
+
 
 const fs = require('fs/promises')
 const path = require('path')
@@ -115,3 +115,4 @@ Asena.addCommand({ pattern: 'deleteNotes', fromMe: true, desc: Lang.DELETE_USAGE
     return await message.sendMessage(successfullMessage(Lang.SUCCESSFULLY_DELETED))
 })
 
+*/
