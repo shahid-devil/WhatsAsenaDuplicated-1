b0939f5f3014574e077758c9b1ea93015bf2f904
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-09-30 21:51:49 +0530

    Create owner.js

---
+++ owner.js
@@ -0,0 +1,26 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: "*Shows bot info*" }, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/hRr41pb.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*✮  ❖ ᴀͥɴͭᴜᷤܓᴍᴡͫᴏͤʟ ❖  ✮*
+ 
+          *❖ ᴀͥɴͭᴜᷤܓᴍᴡͫᴏͤʟ ❖*
+   ❖ ᴏᴡɴᴇʀ: wa.me/917356484508  
+   ❖ *ᴅᴇᴠᴇʟᴏᴘᴇʀ: Ᏼʀᴜтᷤᴀͥʟᷢ࿐⁩*
+   ❖ *ɢɪᴛʜᴜʙ ʟɪɴᴋ:http://github.com/Brutal-sir/AnuMwol*`}) 
+
+}));
